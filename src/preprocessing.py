03022d9c210fd2b1ef9980f5d7ec6f659ed7a77e
Serafín <serapoint@users.noreply.github.com>
2018-01-28 22:18:12 +0000

    First try

---
+++ src/preprocessing.py
@@ -0,0 +1,38 @@
+import string
+import sys
+
+import nltk
+from nltk.stem import *
+
+# Load file and stopwords as taken from command line.
+textfile = open(sys.argv[1], "r")
+stopwordsfile = open(sys.argv[2], "r")
+
+# Read text as a string and stopwords as a list
+text = textfile.read()
+stopwords = stopwordsfile.read().split()
+
+# Case folding & remove punctuation
+text = text.lower()
+text = text.translate(text.maketrans(
+    string.punctuation, " " * len(string.punctuation)))
+
+# Tokenization
+tokens = text.split()
+# print("Length of token list:\t", len(tokens))  # DEBUG
+
+# Stopping
+tokens = [token for token in tokens if token not in stopwords]
+
+# print("Stopwords removed from token list.")  # DEBUG
+# print("Length of token list after stopwords removal:\t", len(tokens))  # DEBUG
+
+# Stemming
+#stemmer = PorterStemmer()
+#stemmed_tokens = [stemmer.stem(token) for token in tokens]
+
+# Write each stemmed token into 'output.txt' in a new line
+outfile = open('output.txt', 'w')
+for token in tokens:
+    outfile.write(token + "\n")
+outfile.close()
---
+++ src/processing.py
@@ -0,0 +1,45 @@
+
+import os
+import re
+import sys
+import xml
+import json
+
+from . import readSubtitles as rs
+
+class Collection():
+    def __init__(self,path):
+        self.path = path
+        self.name = path.split('/')[-1]
+
+    def allwords(self,verbose=True):
+        output_file = open(self.path+'/allwords.txt','w')
+        if verbose:
+            print('Saving all words in '+ self.path+'/allwords.txt')
+
+        for subtitle_path in os.listdir(self.path):
+            subtitle = rs.superSubtitle(self.path + "/" + subtitle_path)
+            if subtitle.extension not in ['srt','sub','txt','tmp','smi','ssa']:
+                if subtitle.name.startswith('.DS_'):
+                    continue
+                raise ValueError('Subtitle extension \''+subtitle.extension+'\' not compatible with superSubtitle. Supersubtitle can only read .srt, .sub, .txt, .tmp, .smi, .ssa subtitles.')
+            subtitle_text = subtitle.toText()
+            output_file.write(subtitle_text+"\n\n")
+
+            if verbose:
+                print('Processed '+ subtitle.name)
+        output_file.close()
+        if verbose:
+            print('Finished.')
+
+def joinAllwords(overall_path):
+    allwords_overall = open('allwords_allseasons.txt','w')
+
+    for season in os.listdir(overall_path):
+        if season.startswith('.DS_S'):
+            continue
+        allwords = open(overall_path+'/'+season+'/allwords.txt','r')
+
+        allwords_overall.write(allwords.read())
+
+    allwords_overall.close()
